07ae17497d0cbf0a1fdf390dc538a37cb4474b38
Ashwinder <sharmaashwinder@gmail.com>
2026-05-20 12:00:44 +0530

    Increased the worker time.

--- docker/queue-worker.sh
+++ docker/queue-worker.sh
@@ -7,8 +7,8 @@ cd /var/www/html
 # Coerce to integers so pcntl_alarm() never receives malformed values from the environment.
 QUEUE_SLEEP=$((${QUEUE_SLEEP:-3} + 0))
 QUEUE_TRIES=$((${QUEUE_TRIES:-3} + 0))
-QUEUE_MAX_TIME=$((${QUEUE_MAX_TIME:-3600} + 0))
-QUEUE_TIMEOUT=$((${QUEUE_TIMEOUT:-120} + 0))
+QUEUE_MAX_TIME=$((${QUEUE_MAX_TIME:-7200} + 0))
+QUEUE_TIMEOUT=$((${QUEUE_TIMEOUT:-300} + 0))
 
 exec php artisan queue:work \
     --sleep="$QUEUE_SLEEP" \
